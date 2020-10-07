fad18c1fcafc9ab898ac2a4c56031ed809e84612
Suarmin Rais <suarmin.rais@gmail.com>
2020-10-07 14:42:08 +0800

    ok

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -103,7 +103,7 @@ class TransaksiController extends Controller
         $tunggakan = Transaksi::leftJoin('tenants', 'transaksis.tenant_id', '=', 'tenants.id')
                     ->leftJoin('penyewas', 'tenants.penyewa_id', '=', 'penyewas.id')
                     ->where('penyewas.nama', 'like', "%$req->kode%")->orWhere('tenants.kode', 'like', "%$req->kode%")
-                    ->where(['transaksis.lokasi_id' => \Auth::user()->lokasi_id, 'transaksis.status' => 'menunggak'])
+                    ->where(['lokasi_id' => \Auth::user()->lokasi_id, 'transaksis.status' => 'menunggak'])
                     ->groupBy('penyewas.nama')
                     ->paginate(20);
         return new TunggakCollection(Tunggak::collection($tunggakan));
